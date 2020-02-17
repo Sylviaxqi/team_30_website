e363df0f02ca82318298b441c551c97111c7d29d
xiaoqi tan <zcabxta@ucl.ac.uk>
2020-02-17 14:45:22 +0000

    Update app.js

--- loginbackend/app.js
+++ loginbackend/app.js
@@ -18,6 +18,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
